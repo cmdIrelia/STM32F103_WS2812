ea5cc0519e9c14d6fd6638e56517342cd0950bb0
cmdIrelia <30620832+cmdIrelia@users.noreply.github.com>
2022-05-24 00:42:54 +0800

    Add files via upload

--- Inc/main.h
+++ Inc/main.h
@@ -58,6 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define WS2812_Pin GPIO_PIN_15
+#define WS2812_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
